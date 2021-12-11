824a82db5b3dab4304d8b5927634479bb89a9f43
Arbaaz Patwari <arbaazpatwari@gmail.com>
2021-12-11 04:23:55 +0000

    update

--- First Website/script.js
+++ First Website/script.js
@@ -3,12 +3,12 @@ function myArray() {
   var person = prompt("Please enter a name", "Arbaaz / Stefan / Mohammed");
   myArray = ["Arbaaz ", "Stefan ", "Mohammed "];
 
-  document.write("Hello "+ myArray[0] + "there are 6 letters in your name.\n");
-  document.write("Hello "+ myArray[1] + "there are 6 letters in your name.\n");
-  document.write("Hello "+ myArray[2] + "there are 8 letters in your name.\n");
+  document.write("Hello " + myArray[0] + " there are 6 letters in your name.\n");
+  document.write("Hello " + myArray[1] + " there are 6 letters in your name.\n");
+  document.write("Hello " + myArray[2] + " there are 8 letters in your name.\n");
 
     document.write(myArray.sort());
 
     document.write(myArray.reverse());
-  
+
 }
